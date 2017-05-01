e59028abbf270036738752e03868efde21731938
Markus Barthlen <doesnotexist@notexistingatall.org>
2017-05-01 11:51:51 +0000

    nothing

--- src/Main.purs
+++ src/Main.purs
@@ -4,10 +4,21 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Node.FS (FS)
-import Node.Buffer (BUFFER)
+import Node.Path as Path
+import Node.FS (FS, FileFlags(..))
+import Node.FS.Stats (statusChangedTime, accessedTime, modifiedTime,
+                      isSymbolicLink, isSocket, isFIFO, isCharacterDevice,
+                      isBlockDevice, isDirectory, isFile)
 import Node.FS.Async as A
-import Node.FS.Sync as S
 
-main ::forall e. Eff (fs :: FS , console :: CONSOLE | e) Unit
+import Network.HTTP (Verb(..))
+
+main::forall e. Eff (fs :: FS , console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
+  let fp = Path.concat
+
+  A.exists (fp ["test", "Test.purs"]) $ \e ->
+    log $ "Test.purs exists? " <> show e
+
+  log $ show DELETE
